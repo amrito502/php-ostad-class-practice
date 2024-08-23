d1f1554bcf8040c3249363c972ef61535dff8ff0
amrito502 <amritobosu7@gmail.com>
2024-08-23 15:05:23 +0600

    str lower, upper, uc

---
+++ s4.php
@@ -0,0 +1,15 @@
+<?php
+$string = "Hello World";
+// lowercase
+
+$lower = strtolower($string);
+echo $lower;
+
+// uppercase
+$upper = strtoupper($string);
+
+echo $upper;
+
+$ucwords = ucwords($string);
+
+echo $ucwords;  
